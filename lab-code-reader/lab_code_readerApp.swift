2a6cb2c671ae68963ed6f6e839d839ab0dd3d54e
Carolyn Aquino <caquino1223@gmail.com>
2025-11-12 20:59:07 -0500

    Initial Commit

---
+++ lab-code-reader/lab_code_readerApp.swift
@@ -0,0 +1,17 @@
+//
+//  lab_code_readerApp.swift
+//  lab-code-reader
+//
+//  Created by Carolyn Aquino on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct lab_code_readerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
